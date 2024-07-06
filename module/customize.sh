281e1d1127eec94045baea2da1b06f6faebc7b4b
Wang Han <416810799@qq.com>
2024-07-06 22:28:23 +0800

    Downgrade minSdk to 30

--- module/customize.sh
+++ module/customize.sh
@@ -1,6 +1,6 @@
 # Error on < Android 14.
-if [ "$API" -lt 34 ]; then
-    abort "- !!! You can't use this module on Android < 14"
+if [ "$API" -lt 30]; then
+    abort "- !!! You can't use this module on Android < 11"
 fi
 
 # safetynet-fix module is obsolete and it's incompatible with PIF.
